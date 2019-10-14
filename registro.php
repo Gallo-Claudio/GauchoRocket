00594725c2684b2e37d549c94a5e35de49912475
julieta <julieta.iracheta@hotmail.com>
2019-10-14 18:26:33 -0300

    html buscador de vuelos

--- registro.php
+++ registro.php
@@ -1,64 +1,85 @@
 <?php
-session_start();
-require 'conexion.php';
+    $error="";
+    $usuario="";
+    $clase="";
 
-if (isset($_POST['enviar'])) {
-    $usuario = $_POST['usuario'];
-    $clave = $_POST['clave'];
-    $clave2 = $_POST['clave2'];
+    require 'conexion.php';
 
+    if (isset($_POST['enviar'])) {
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['clave'];
+        $clave2 = $_POST['clave2'];
 
-$sql = "SELECT * FROM usuarios as user WHERE usuario = '$usuario' AND clave = '$clave' AND clave2 = '$clave2'";
-$consulta = mysqli_query($conexion,$sql);
-$array = mysqli_fetch_array($consulta);
+        if(empty($usuario) or empty($clave)){
+            $error = "<div class='w3-panel w3-red'><p>Nombre/Contraseña<br>NO pueden estar vacios</p></div>";
+            $clase ="animated shake";
+        }else{
 
+            $sql= "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+            $resultado = mysqli_query($conexion,$sql);
+            $lista = mysqli_fetch_all($resultado);
 
-}
+            if(!empty($lista)){
+                $error = "<div class='w3-panel w3-red'><p>El nombre de usuario ya existe</p></div>";
+                $clase ="animated shake";
+            }else {
 
+                if ($clave == $clave2) {
+                    $sql = "INSERT INTO usuarios (usuario, clave, rol) VALUES ('$usuario','$clave','2')";
+                    $consulta = mysqli_query($conexion, $sql);
 
+                    if (!$consulta) {
+                        $error = "<div class='w3-panel w3-red'><p>ERROR<br>No se pudieron guardar los datos</p></div>";
+                        $clase ="animated shake";
+                    } else {
+                        $usuario = "";
+                        $error = "<div class='w3-panel w3-light-green'><p>Usuario ingresado!!</p></div>";
+                    }
 
+                } else {
+                    $error = "<div class='w3-panel w3-red'><p>La contraseña no coincide<br>Vuelva a tipearla</p></div>";
+                    $clase ="animated shake";
+                }
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Iniciar sesion</title>
+    <title>Registro</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link rel="stylesheet" href="css/gr.css">
 </head>
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
-<style>
-    .w3-lobster {
-        font-family: "Lobster", serif;
-    }
-</style>
 <body>
 
 <body>
-    <div class="w3-container w3-lobster">
-        <center> <p class="w3-xxxlarge">Bienvenido al registro</p></center>
-
-<center>
-
-    <form class="w3-container w3-card-4" method="POST" action="registro.php" >
-
-        <label class="w3-xlarge"><b>Nombre:</b></label>
-        <input class="w3-xlarge" type="text" name="usuario"><br><br>
-
-        <label class="w3-xlarge"><b>Contraseña:</b></label>
-        <input class="w3-xlarge" type="password" name="clave"><br><br>
-
-        <label class="w3-xlarge"><b>Repita su contraseña:</b></label>
-        <input class="w3-xlarge" type="password" name="clave2"><br><br>
-
-        <button class="w3-button w3-black w3-round-xlarge" type="button" name="enviar"><a href="login.php">Registrarse</a></button><br><br>
-        <button class="w3-button w3-black w3-round-xlarge" name="iniciosesion"><a href="login.php">Iniciar sesion</button>
-    </form><br>
-
-
-</center>
+<div class="w3-container w3-lobster banda">
+    <p class="w3-xxxlarge w3-center">Registro a Gaucho Rocket</p><img src="img/cohete-espacial-mini.png" class="animated bounceInUp">
 </div>
-</body>
-</html>
+
+<div class="w3-display-container">
+        <form class="w3-container w3-card-4 w3-content login <?php echo $clase; ?>" method="POST" action="registro.php" >
+
+            <?php echo $error; ?>
+            <label class="w3-xlarge w3-lobster">Nombre:</label>
+            <input class="w3-input w3-margin-bottom w3-hover-gray" type="text" name="usuario" value="<?php echo$usuario; ?>">
+
+            <label class="w3-xlarge w3-lobster">Contraseña:</label>
+            <input class="w3-input w3-margin-bottom w3-hover-gray" type="password" name="clave">
+
+            <label class="w3-xlarge w3-lobster">Repita su contraseña:</label>
+            <input class="w3-input w3-margin-bottom w3-hover-gray" type="password" name="clave2">
+
+            <button class="w3-button w3-round-xlarge w3-green derecha" type="submit" name="enviar">Registrarse</button><br><br>
+            <button class="w3-button w3-round-xlarge w3-dark-grey derecha" name="iniciosesion"><a href="login.php">Iniciar sesion</button>
+        </form>
+</div>
+<?php
+include "pie.html";
+?>
---
+++ vuelo.php
@@ -0,0 +1,102 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+        <meta charset="UTF-8">
+        <title>Busca tu vuelo</title>
+        <meta charset="UTF-8">
+        <title>Inicio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
+        <link rel="stylesheet" href="css/gr.css">
+</head>
+<body>
+
+
+<div class="w3-container w3-lobster banda">
+    <p class="w3-xxxlarge w3-center">Busca tu vuelo ideal<img src="img/cohete-espacial-mini.png"></p>
+    <a href='salir.php' class="w3-button w3-round-xlarge w3-red derecha">Salir</a>
+</div>
+
+
+    <form action="vuelo.php" method="post" target="_blank">
+
+        <center>Origen:  <select name="selectO">
+                <option value="value1">Buenos Aires</option>
+                <option value="value2" selected>Ankara</option>
+            </select></center><br>
+
+
+
+     <center>Destino:  <select name="selectD">
+            <option value="value1">Estación Espacial Internacional</option>
+            <option value="value2">OrbiterHotel</option>
+            <option value="value3" selected>Luna </option>
+            <option value="value4">Marte</option>
+            <option value="value4">Ganimedes</option>
+            <option value="value4">Europa</option>
+            <option value="value4">Io</option>
+            <option value="value4">Encedalo</option>
+            <option value="value4">Titán</option>
+
+         </select></center><br><br>
+
+        <center> <label for="start">Fecha de salida</label>
+
+            <input type="date" name="calendario"
+                   value="2018-07-22"
+                   min="2019-12-12" max="2021-01-01"<br>
+
+            <label for="start">Fecha de llegada</label>
+
+            <input type="date" name="calendario"
+                   value="2018-07-22"
+                   min="2019-12-12" max="2021-01-01"<br></center><br><br>
+
+
+
+        <center>Tipo de vuelo:  <select name="tipoVuelo">
+                <option value="value1">Suborbital</option>
+                <option value="value2" selected>Orbital</option>
+                <option value="value2" selected>Entre destino</option>
+            </select></center><br>
+        <center><p class="w3-xxxlarge w3-center"><h3>Cantidad de adultos: <input type="number" name="cantidada" min="1" max="20" step="1"></h3></p></center>
+        <center><p class="w3-xxxlarge w3-center"><h3>Cantidad de menores: <input type="number" name="cantidadm" min="1" max="20" step="1"></h3></p></center>
+
+
+
+        <center><input class="w3-button w3-round-xlarge w3-dark-grey" type="submit" value="aceptar"></center>
+
+
+
+
+
+    </form>
+
+
+
+</body>
+
+<div class="w3-display-container">
+</div>
+<?php
+include "pie.html";
+?>
