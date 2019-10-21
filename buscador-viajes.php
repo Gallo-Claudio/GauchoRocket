037ea96752e0f9b2c3addff76331beb20c24139f
Gallo <g>
2019-10-21 12:18:20 -0300

    Corrección de la conexión a la Bd

--- buscador-viajes.php
+++ buscador-viajes.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("127.0.0.1","root","46598842","gauchorocket");
+$conexion = mysqli_connect("127.0.0.1:3306","pw2","pw22019","gauchorocket");
 if(!$conexion){
     echo "<p>ERROR de conexion a la BD</p>";
     die;
@@ -10,13 +10,15 @@ $registro_tipo_viajes = mysqli_fetch_all($resultado_tipo_viajes);
 $fecha_actual = date("Y-m-d");
 $fecha_minimo = date("Y-m-d",strtotime($fecha_actual."+ 1 days"));
 $error_fecha = "";
+
 if (isset($_POST['enviar'])) {
     $fecha_salida = $_POST['fecha_salida'];
     $hora_salida_inicial = $_POST['hora_salida_inicial'];
     $hora_salida_final = $_POST['hora_salida_final'];
     $tipo_viajes = $_POST['tipo_viajes'];
+
     if ($fecha_salida == "" && $fecha_salida < $fecha_minimo) {
-        $error_fecha = "<center><p class=\"w3-xlarge w3-lobster\">Ingrese una fecha valida</p></center>";
+        $error_fecha = "Ingrese una fecha valida";
     } else {
         $sql = "select fecha_salida, hora_salida, tipo_viajes.tipo_viaje, duracion from viajes as v
                     inner join tipo_viajes
@@ -28,6 +30,7 @@ if (isset($_POST['enviar'])) {
             if ($hora_salida_inicial < $hora_salida_final) {
                 $sql = $sql . " AND hora_salida between '$hora_salida_inicial' and '$hora_salida_final'";
             }
+
         }
         if($tipo_viajes != "-"){
             $sql = $sql . " AND v.tipo_viaje = '$tipo_viajes'";
@@ -35,12 +38,15 @@ if (isset($_POST['enviar'])) {
         $resultado = mysqli_query($conexion, $sql);
     }
 }
+
 /*  where v.dia = '$dias' and v.hora='$horas' and v.tipo_viaje = '$tipo_viajes'";
 $resultado = mysqli_query($conexion, $sql);
 $lista = mysqli_fetch_all($resultado);
+
 if (empty($lista)) {
  $error = "<p class='error animated shake'>No se ha encontrado ningún viaje</p>";
 }*/
+
 ?>
 
 <!DOCTYPE html>
@@ -57,121 +63,119 @@ if (empty($lista)) {
     <?php include "header.php" ?>
 </head>
 <body>
-
-<div class="w3-container w3-lobster banda">
-    <p class="w3-xxxlarge w3-center">Buscador de vuelos</p><img src="img/cohete-espacial-mini.png" class="animated bounceInUp">
-</div>
-
 <form method="POST" action="buscador-viajes.php">
     <?php echo "<p>$error_fecha</p>"; ?>
 
-    <center><div class="selector"><label class="w3-xlarge w3-lobster" for='fecha_salida'>Fecha Salida:</label>
+    <div class="selector"><label for='fecha_salida'>Fecha Salida:</label>
 
-            <input name="fecha_salida"type="date" min="<?php echo $fecha_minimo?>">
+        <input name="fecha_salida"type="date" min="<?php echo $fecha_minimo?>">
 
-        </div></center>
+    </div>
 
-    <center> <div class="selector"><label class="w3-xlarge w3-lobster">Hora salida:</label>
-            <select name="hora_salida_inicial">
-                <option value="-">--:--</option>
-                <option value="00:00">00:00</option>
-                <option value="01:00">01:00</option>
-                <option value="02:00">02:00</option>
-                <option value="03:00">03:00</option>
-                <option value="04:00">04:00</option>
-                <option value="05:00">05:00</option>
-                <option value="06:00">06:00</option>
-                <option value="07:00">07:00</option>
-                <option value="08:00">08:00</option>
-                <option value="09:00">09:00</option>
-                <option value="10:00">10:00</option>
-                <option value="11:00">11:00</option>
-                <option value="12:00">12:00</option>
-                <option value="13:00">13:00</option>
-                <option value="14:00">14:00</option>
-                <option value="15:00">15:00</option>
-                <option value="16:00">16:00</option>
-                <option value="17:00">17:00</option>
-                <option value="18:00">18:00</option>
-                <option value="19:00">19:00</option>
-                <option value="20:00">20:00</option>
-                <option value="21:00">21:00</option>
-                <option value="22:00">22:00</option>
-                <option value="23:00">23:00</option>
-            </select>
+    <div class="selector"><label>Hora salida:</label>
+        <select name="hora_salida_inicial">
+            <option value="-">--:--</option>
+            <option value="00:00">00:00</option>
+            <option value="01:00">01:00</option>
+            <option value="02:00">02:00</option>
+            <option value="03:00">03:00</option>
+            <option value="04:00">04:00</option>
+            <option value="05:00">05:00</option>
+            <option value="06:00">06:00</option>
+            <option value="07:00">07:00</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            <option value="22:00">22:00</option>
+            <option value="23:00">23:00</option>
+        </select>
+    </div>
+    <div class="selector">
+        <select name="hora_salida_final">
+            <option value="-">--:--</option>
+            <option value="01:00">01:00</option>
+            <option value="02:00">02:00</option>
+            <option value="03:00">03:00</option>
+            <option value="04:00">04:00</option>
+            <option value="05:00">05:00</option>
+            <option value="06:00">06:00</option>
+            <option value="07:00">07:00</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            <option value="22:00">22:00</option>
+            <option value="23:00">23:00</option>
+            <option value="23:59">23:59</option>
+        </select>
+    </div>
 
-            <select name="hora_salida_final">
-                <option value="-">--:--</option>
-                <option value="01:00">01:00</option>
-                <option value="02:00">02:00</option>
-                <option value="03:00">03:00</option>
-                <option value="04:00">04:00</option>
-                <option value="05:00">05:00</option>
-                <option value="06:00">06:00</option>
-                <option value="07:00">07:00</option>
-                <option value="08:00">08:00</option>
-                <option value="09:00">09:00</option>
-                <option value="10:00">10:00</option>
-                <option value="11:00">11:00</option>
-                <option value="12:00">12:00</option>
-                <option value="13:00">13:00</option>
-                <option value="14:00">14:00</option>
-                <option value="15:00">15:00</option>
-                <option value="16:00">16:00</option>
-                <option value="17:00">17:00</option>
-                <option value="18:00">18:00</option>
-                <option value="19:00">19:00</option>
-                <option value="20:00">20:00</option>
-                <option value="21:00">21:00</option>
-                <option value="22:00">22:00</option>
-                <option value="23:00">23:00</option>
-                <option value="23:59">23:59</option>
-            </select>
-        </div></center>
-
-    <center><div class="selector"><label class="w3-xlarge w3-lobster" for='tipo_viajes'>Tipo de viaje:</label>
-            <select name='tipo_viajes'>
-                <option value="-">-</option>
-                <?php
-                $tv=0;
-                while($tv < count($registro_tipo_viajes)) {
-                    echo"<option value='".$registro_tipo_viajes[$tv][0]."'>".$registro_tipo_viajes[$tv][1]."</option>";
-                    $tv++;
-                }
-                ?>
-            </select></div></center><br>
+    <div class="selector"><label for='tipo_viajes'>Tipo de viaje:</label>
+        <select name='tipo_viajes'>
+            <option value="-">-</option>
+            <?php
+            $tv=0;
+            while($tv < count($registro_tipo_viajes)) {
+                echo"<option value='".$registro_tipo_viajes[$tv][0]."'>".$registro_tipo_viajes[$tv][1]."</option>";
+                $tv++;
+            }
+            ?>
+        </select></div>
 
 
-    <center><button class="w3-button w3-round-xlarge w3-red" type="submit" name="enviar">Buscar</button></center>
+    <button type="submit" name="enviar">Buscar</button>
 </form>
 <?php
+
 if ($error_fecha == "") {
     if (mysqli_affected_rows($conexion) > 0) {
         echo "<table>
                 <thead>
                   <tr>
-                    <td class=\"w3-xlarge w3-lobster\">Fecha</td>
-                    <td class=\"w3-xlarge w3-lobster\">Hora</td>
-                    <td class=\"w3-xlarge w3-lobster\">Tipo de viaje</td>
-                    <td class=\"w3-xlarge w3-lobster\">Duracion</td>
+                    <td>Fecha</td>
+                    <td>Hora</td>
+                    <td>Tipo de viaje</td>
+                    <td>Duracion</td>
                   </tr>
                  </thead>
-                 
                  <tbody>";
-        while ($lista = mysqli_fetch_assoc($resultado)) {
-            echo "<tr>
-                    <td class=\"w3-large w3-lobster\">" . $lista['fecha_salida'] . "</td>
-                    <td class=\"w3-large w3-lobster\">" . $lista['hora_salida'] . "</td>
-                    <td class=\"w3-large w3-lobster\">" . $lista['tipo_viaje'] . "</td>
-                    <td class=\"w3-large w3-lobster\">" . $lista['duracion'] . "</td>
-                    <td><center><button class=\"w3-button w3-round-xlarge w3-green\" type='submit' name='aceptar'><a href=\"reservas.php\">Reservar</button></center></td>
 
+
+        while ($lista = mysqli_fetch_assoc($resultado)) {
+
+
+            echo "<tr>
+                    <td>" . $lista['fecha_salida'] . "</td>
+                    <td>" . $lista['hora_salida'] . "</td>
+                    <td>" . $lista['tipo_viaje'] . "</td>
+                    <td>" . $lista['duracion'] . "</td>
                    </tr>";
 
         }
         echo "</tbody></table>";
     } else {
-        echo "<center><p class=\"w3-xlarge w3-lobster\">No se encontraron vuelos disponibles</p></center>";
+        echo "<p>No se encontraron vuelos disponibles</p>";
     }
 }
 ?>
