de52fcaa2dcd678fa1a5f153541a7be88abda010
Gallo <g>
2019-12-03 11:22:00 -0300

    Fecha busqueda

--- buscador.php
+++ buscador.php
@@ -13,8 +13,8 @@ $sql_estaciones= "select * from estaciones";
 $resultado_estaciones = mysqli_query($conexion, $sql_estaciones);
 $registro_estaciones = mysqli_fetch_all($resultado_estaciones);
 //$fecha_actual = date("Y-m-d H:i:s");
-//$fecha_minimo = date("Y-m-d",strtotime($fecha_actual."+ 1 days"));
-$fecha_minimo = date("Y-m-d H:i:s");
+$fecha_minimo = date("Y-m-d",strtotime($fecha_actual."+ 0 days"));
+//$fecha_minimo = date("Y-m-d H:i:s");
 $error_fecha = "";
 
 ?>
